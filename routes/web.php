748502f7512a5fff0dea737472f2a0b3c3bb91d1
Constantinely <43109874+Constantinely@users.noreply.github.com>
2020-08-25 18:18:53 +0300

    sign up form and logic

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware'=>['web']], function(){     
+    Route::get('/', function () {
+        return view('welcome');
+    });
+
+    Route::post('/signup', [
+        'uses' => 'UserController@postSignUp',
+        'as' => 'signup'
+    ]);
 });
